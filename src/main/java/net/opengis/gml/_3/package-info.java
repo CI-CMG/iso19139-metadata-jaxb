dd01ef2d82c5af6829049bc27f730e96237e7018
max smith <maxwellfth@gmail.com>
2025-09-15 13:21:04 -0600

    migrate javax to jakarta and update java

--- src/main/java/net/opengis/gml/_3/package-info.java
+++ src/main/java/net/opengis/gml/_3/package-info.java
@@ -1,9 +1,9 @@
-@XmlSchema(namespace = "http://www.opengis.net/gml/3.2", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
+@XmlSchema(namespace = "http://www.opengis.net/gml/3.2", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED,
     xmlns = {
         @XmlNs(namespaceURI = "http://www.opengis.net/gml/3.2", prefix = "gml"),
         @XmlNs(namespaceURI = "http://www.w3.org/1999/xlink", prefix = "xlink")
 })
 package net.opengis.gml._3;
 
-import javax.xml.bind.annotation.XmlNs;
-import javax.xml.bind.annotation.XmlSchema;
+import jakarta.xml.bind.annotation.XmlNs;
+import jakarta.xml.bind.annotation.XmlSchema;
--- src/main/java/org/isotc211/_2005/gts/package-info.java
+++ src/main/java/org/isotc211/_2005/gts/package-info.java
@@ -1,9 +1,9 @@
-@XmlSchema(namespace = "http://www.isotc211.org/2005/gts", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
+@XmlSchema(namespace = "http://www.isotc211.org/2005/gts", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED,
     xmlns = {
         @XmlNs(namespaceURI = "http://www.isotc211.org/2005/gts", prefix = "gts"),
         @XmlNs(namespaceURI = "http://www.w3.org/1999/xlink", prefix = "xlink")
 })
 package org.isotc211._2005.gts;
 
-import javax.xml.bind.annotation.XmlNs;
-import javax.xml.bind.annotation.XmlSchema;
+import jakarta.xml.bind.annotation.XmlNs;
+import jakarta.xml.bind.annotation.XmlSchema;
